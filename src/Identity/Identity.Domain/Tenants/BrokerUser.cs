d8dce7d466283d4c514cfd74016eba6fe82bcfb6
Janitha Tennakoon <janitha@randomsoftware.net>
2026-08-25 16:22:44 +0530

    [ADD] Identity domain

---
+++ src/Identity/Identity.Domain/Tenants/BrokerUser.cs
@@ -0,0 +1,12 @@
+namespace Identity.Domain.Tenants;
+
+public class BrokerUser
+{
+    public Guid Id { get; set; }
+    public Guid TenantId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
